b5a6a93b0cf3f3931346d585e5ca77ac62994f25
Ameeq-coder <ahmadameeq19@gmail.com>
2024-08-10 22:49:08 +0500

    Bug resloved

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const User = require("../models/user.model");
+const bcrypt = require('bcryptjs');
+const User = require("./models/user.model");
 
 const router = express.Router();
 
