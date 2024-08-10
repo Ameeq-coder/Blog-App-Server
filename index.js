4bdd68aa7b30662ec4425cda14ccc966e6d23765
Ameeq-coder <ahmadameeq19@gmail.com>
2024-08-11 01:47:54 +0500

    altas

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;  // Use environment variable for port
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-    
-});
 
+mongoose.connect("mongodb+srv://ameeqahmad05:ameeq25012003@blogapp.f1jim.mongodb.net/?retryWrites=true&w=majority&appName=blogapp", {
+ 
+});
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
