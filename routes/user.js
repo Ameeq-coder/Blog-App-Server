29c2b150c78567fd3e3d7ebdab93291b57d6f2b6
Ameeq-coder <ahmadameeq19@gmail.com>
2024-08-10 19:35:29 +0500

    Your commit message

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,29 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     const { email, password, confirmpass } = req.body;
 
     if (password !== confirmpass) {
         return res.status(400).json({ msg: "Password and confirm password do not match" });
     }
 
-    const user = new User({
-        email,
-        password,
-        confirmpass
-    });
-
-    user
-        .save()
-        .then(() => {
-            console.log("User registered");
-            res.status(200).json("User registered successfully");
-        })
-        .catch((err) => {
-            res.status(403).json({ msg: err });
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            email,
+            password: hashedPassword,
         });
+
+        await user.save();
+        console.log("User registered");
+        res.status(200).json("User registered successfully");
+    } catch (err) {
+        res.status(403).json({ msg: err.message });
+    }
 });
 
 module.exports = router;
